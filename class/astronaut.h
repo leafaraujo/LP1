12f080926e27717b1fe6ea9a1fde3c2aed0a61f6
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-04-11 18:22:07 -0300

    Create astronaut.h

---
+++ class/astronaut.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string>
+
+class Astronaut {
+    private:
+    std::string name;
+    int age;
+    int cpf;
+    char gender;
+    int medals;
+    int status;
+
+    public:
+    getName
+}
