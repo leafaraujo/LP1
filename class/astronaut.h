ba1604abd54fcb3f30aeb541c84b4c95da9cbfb9
Rafael Araújo <137319648+leafaraujo@users.noreply.github.com>
2024-04-16 14:54:43 -0300

    Update astronaut.h

--- class/astronaut.h
+++ class/astronaut.h
@@ -11,9 +11,13 @@ class Astronaut {
     public:
         Astronaut(const std::string& _name, const std::string& _cpf, const std::string& _gender,
               int _age, int _medals = 0, int _status = 1);
-
-        void saveAstronautaData(const Astronaut& astronaut);
-
+        const std::string& getName() const;
+        const std::string& getCPF() const;
+        const std::string& getGender() const;
+        const int getAge() const;
+        const int getMedals() const;
+        const int getStatus() const;
+        void saveAstronautData(const Astronaut& astronaut);
 };
 
 #endif
